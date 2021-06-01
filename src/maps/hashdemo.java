1a05df37b5ed34629116ba2cb3e4783519e66093
ChanHyukPark-Tech <pgg6713@gmail.com>
2021-06-01 11:09:04 +0900

    hash table_

---
+++ src/maps/hashdemo.java
@@ -0,0 +1,43 @@
+package maps;
+
+
+import java.util.Objects;
+
+class Key{
+    public int value;
+
+    public Key(int value){
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Key key = (Key) o;
+        return value == key.value;
+    }
+
+    @Override
+    public int hashCode() {
+        int low = value & 0xffff;
+        int high = value >>> 16 & 0x0ffff;
+//        System.out.println("low : " + low);
+//        System.out.println("high : " + high);
+//        System.out.println(0x0ffff);
+        return (low + high) & 0x0ffff;
+    }
+}
+
+public class hashdemo{
+    public static void main(String[] args) {
+        Key foo = new Key(1234);
+
+        System.out.println(foo);
+
+        System.out.println(
+                Integer.toHexString(foo.hashCode())
+        );
+        System.out.println(foo.hashCode()       );
+    }
+}
